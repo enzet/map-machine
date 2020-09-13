a3bafc8c84c162499b1038fe687a1e0feb6b52ed
Sergey Vartanov <me@enzet.ru>
2020-09-13 21:23:10 +0300

    Add direction tests.

--- test/test_direction.py
+++ test/test_direction.py
@@ -20,3 +20,11 @@ def test_compass_points_2():
 def test_compass_points_3():
     assert np.allclose(
         parse_vector("SSW"), np.array([-0.38268343, 0.92387953]))
+
+
+def test_wrong():
+    assert not parse_vector("O")
+
+
+def test_degree():
+    assert np.allclose(parse_vector("90"), np.array([1, 0]))
