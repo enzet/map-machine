b776db6bd24867ddd26126b5f39f0d2cb9561611
Sergey Vartanov <me@enzet.ru>
2021-04-24 17:47:21 +0300

    Fix code style.

--- roentgen/color.py
+++ roentgen/color.py
@@ -1,5 +1,5 @@
 """
-Röntgen project. Color utility.
+Color utility.
 
 Author: Sergey Vartanov (me@enzet.ru)
 """
@@ -12,14 +12,17 @@ from roentgen.util import MinMax
 
 def is_bright(color: Color) -> bool:
     """
-    Is color bright enough to have black outline instead of white.
+    Check whether color bright enough to have black outline instead of white.
     """
     return (
         0.2126 * color.red + 0.7152 * color.green + 0.0722 * color.blue
-        > 0.78125)
+        > 0.78125
+    )
 
 
-def get_gradient_color(value: Any, bounds: MinMax, colors: List[Color]):
+def get_gradient_color(
+        value: Any, bounds: MinMax, colors: List[Color]
+    ) -> Color:
     """
     Get color from the color scale for the value.
 
@@ -28,15 +31,19 @@ def get_gradient_color(value: Any, bounds: MinMax, colors: List[Color]):
     :param colors: color scale
     """
     color_length: int = len(colors) - 1
-    scale = colors + [Color("black")]
+    scale: List[Color] = colors + [Color("black")]
 
     coefficient: float = (
         0 if bounds.max_ == bounds.min_ else
-        (value - bounds.min_) / (bounds.max_ - bounds.min_))
+        (value - bounds.min_) / (bounds.max_ - bounds.min_)
+    )
     coefficient = min(1.0, max(0.0, coefficient))
     index: int = int(coefficient * color_length)
-    color_coefficient = (coefficient - index / color_length) * color_length
+    color_coefficient: float = (
+        (coefficient - index / color_length) * color_length
+    )
 
     return Color(rgb=[
         scale[index].rgb[i] + color_coefficient *
-        (scale[index + 1].rgb[i] - scale[index].rgb[i]) for i in range(3)])
+        (scale[index + 1].rgb[i] - scale[index].rgb[i]) for i in range(3)
+    ])
