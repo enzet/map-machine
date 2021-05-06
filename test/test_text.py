330648e03445e2b2cca348135e41371890f30fab
Sergey Vartanov <me@enzet.ru>
2021-05-06 23:32:25 +0300

    Add more label tests.

--- test/test_text.py
+++ test/test_text.py
@@ -13,3 +13,16 @@ def test_1_label() -> None:
     labels = get_text({"name": "Name"})
     assert len(labels) == 1
     assert labels[0].text == "Name"
+
+
+def test_1_label_unknown_tags() -> None:
+    labels = get_text({"name": "Name", "aaa": "bbb"})
+    assert len(labels) == 1
+    assert labels[0].text == "Name"
+
+
+def test_2_labels() -> None:
+    labels = get_text({"name": "Name", "ref": "5"})
+    assert len(labels) == 2
+    assert labels[0].text == "Name"
+    assert labels[1].text == "5"
