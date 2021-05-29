cb988db0338cf98cfbd03b77d050dcbccbded3f2
Sergey Vartanov <me@enzet.ru>
2021-05-29 21:52:38 +0300

    Add gradient test.

--- test/test_color.py
+++ test/test_color.py
@@ -3,7 +3,8 @@ Test color functions.
 """
 from colour import Color
 
-from roentgen.color import is_bright
+from roentgen.color import is_bright, get_gradient_color
+from roentgen.util import MinMax
 
 __author__ = "Sergey Vartanov"
 __email__ = "me@enzet.ru"
@@ -18,3 +19,12 @@ def test_is_bright() -> None:
     assert not is_bright(Color("brown"))
     assert not is_bright(Color("black"))
 
+
+def test_gradient() -> None:
+    """
+    Test color picking from gradient.
+    """
+    assert (
+        get_gradient_color(0.5, MinMax(0, 1), [Color("black"), Color("white")])
+        == Color("#7F7F7F")
+    )
