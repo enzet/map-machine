ec389be1aedbf5017537c777ee8e67f8075e86a8
Sergey Vartanov <me@enzet.ru>
2021-08-15 06:09:44 +0300

    Add test for MapCSS generation.

---
+++ test/test_mapcss.py
@@ -0,0 +1,27 @@
+"""
+Test MapCSS generation.
+"""
+from roentgen.mapcss import MapCSSWriter
+from roentgen.scheme import NodeMatcher
+from test import SCHEME
+
+__author__ = "Sergey Vartanov"
+__email__ = "me@enzet.ru"
+
+
+def test_mapcss() -> None:
+    """
+    Test MapCSS generation.
+    """
+    writer: MapCSSWriter = MapCSSWriter(SCHEME, "icons", False)
+    matcher: NodeMatcher = NodeMatcher(
+        {"tags": {"natural": "tree"}, "shapes": ["tree"]}
+    )
+    selector = writer.add_selector("node", matcher)
+    assert (
+        selector
+        == """node[natural="tree"] {
+    icon-image: "icons/tree.svg";
+}
+"""
+    )
