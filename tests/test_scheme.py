8b9748087fa4253b820c978c9d00c762a42721b3
Sergey Vartanov <me@enzet.ru>
2022-01-15 12:51:56 +0300

    Fix scheme test.

--- tests/test_scheme.py
+++ tests/test_scheme.py
@@ -6,13 +6,23 @@ from map_machine.scheme import Scheme
 
 def test_verification() -> None:
     assert (
-        Scheme({"node_icons": [{"tags": [{"tags": {"a": "b"}}]}]})
+        Scheme(
+            {
+                "colors": {"default": "#444444"},
+                "node_icons": [{"tags": [{"tags": {"a": "b"}}]}],
+            }
+        )
         .node_matchers[0]
         .verify()
         is True
     )
     assert (
-        Scheme({"node_icons": [{"tags": [{"tags": {"a": 0}}]}]})
+        Scheme(
+            {
+                "colors": {"default": "#444444"},
+                "node_icons": [{"tags": [{"tags": {"a": 0}}]}],
+            }
+        )
         .node_matchers[0]
         .verify()
         is False
