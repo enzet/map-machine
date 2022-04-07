a554d28c5c099e8ecb68910a1500ef9d3969f0a0
Sergey Vartanov <me@enzet.ru>
2022-04-08 00:10:55 +0300

    Refactor scheme test.

--- tests/test_scheme.py
+++ tests/test_scheme.py
@@ -1,29 +1,24 @@
 """
 Test scheme parsing.
 """
+from typing import Any
+
 from map_machine.scheme import Scheme
 
 
 def test_verification() -> None:
-    assert (
-        Scheme(
-            {
-                "colors": {"default": "#444444"},
-                "node_icons": [{"tags": [{"tags": {"a": "b"}}]}],
-            }
-        )
-        .node_matchers[0]
-        .verify()
-        is True
-    )
-    assert (
-        Scheme(
-            {
-                "colors": {"default": "#444444"},
-                "node_icons": [{"tags": [{"tags": {"a": 0}}]}],
-            }
-        )
-        .node_matchers[0]
-        .verify()
-        is False
-    )
+    """Test verification process of tags in scheme."""
+
+    tags: dict[str, Any] = {
+        "colors": {"default": "#444444"},
+        "node_icons": [{"tags": [{"tags": {"a": "b"}}]}],
+    }
+    assert Scheme(tags).node_matchers[0].verify() is True
+
+    # Tag value should be string, not integer.
+
+    tags: dict[str, Any] = {
+        "colors": {"default": "#444444"},
+        "node_icons": [{"tags": [{"tags": {"a": 0}}]}],
+    }
+    assert Scheme(tags).node_matchers[0].verify() is False
