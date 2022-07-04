93605c81f5a8aa36b82e71b9c04158973114ca10
Sergey Vartanov <me@enzet.ru>
2022-07-04 03:18:10 +0300

    Rewrite requirements list.
    Get requirements from `requirements.txt` instead of having two lists.

--- map_machine/__init__.py
+++ map_machine/__init__.py
@@ -11,17 +11,7 @@ __author__ = "Sergey Vartanov"
 __email__ = "me@enzet.ru"
 __version__ = "0.1.6"
 
-REQUIREMENTS: list[str] = [
-    "CairoSVG>=2.5.0",
-    "colour>=0.1.5",
-    "numpy>=1.18.1",
-    "Pillow>=8.2.0",
-    "portolan>=1.0.1",
-    "pycairo>=1.20.1",
-    "pytest>=6.2.2",
-    "PyYAML>=4.2b1",
-    "setuptools>=51.0.0",
-    "Shapely>=1.7.1",
-    "svgwrite>=1.4",
-    "urllib3>=1.25.6",
-]
+from pathlib import Path
+
+with Path("requirements.txt").open() as input_file:
+    REQUIREMENTS: list[str] = [x[:-1] for x in input_file.readlines()]
